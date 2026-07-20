6502e78d0bbf02a4e91f2ed62584ecb9e8198590
Tiana White <tianaw@ocadu.ca>
2026-07-19 20:35:22 -0400

    Add folder with all files

---
+++ Java/script.js
@@ -0,0 +1,55 @@
+function GDNav(){
+    document.body.style.backgroundColor= "rgb(255, 215, 184)"; 
+    setTimeout(function(){
+        window.location.href ='GD.html'; 
+    }, 150); 
+}
+
+function TPNav(){
+     document.body.style.backgroundColor= "rgb(241, 214, 245);"; 
+    setTimeout(function(){
+        window.location.href ='TP.html'; 
+    }, 150); 
+}
+window.addEventListener('pageshow', function(event){
+    if (event.persisted){
+        document.body.style.backgroundColor = ''; 
+    }
+})
+function toggleParagraph(){
+    var description = document.getElementById("piece-description"); 
+    description.classList.toggle("show"); 
+    console.log("current classes on description:", description.className); 
+}
+function toggleParagraph2(){
+    var description = document.getElementById("piece-description2"); 
+    description.classList.toggle("show"); 
+    console.log("current classes on description:", description.className); 
+}
+function toggleParagraph3(){
+    var description = document.getElementById("piece-description3"); 
+    description.classList.toggle("show"); 
+    console.log("current classes on description:", description.className); 
+}
+function toggleParagraph4(){
+    var description = document.getElementById("piece-description4"); 
+    description.classList.toggle("show"); 
+    console.log("current classes on description:", description.className); 
+}
+function toggleParagraph5(){
+    var description = document.getElementById("piece-description5"); 
+    description.classList.toggle("show"); 
+    console.log("current classes on description:", description.className); 
+}
+function toggleParagraph6(){
+    var description = document.getElementById("piece-description6"); 
+    description.classList.toggle("show"); 
+    console.log("current classes on description:", description.className); 
+}
+function toggleParagraph7(){
+    var description = document.getElementById("piece-description7"); 
+    description.classList.toggle("show"); 
+    console.log("current classes on description:", description.className); 
+}
+
+
